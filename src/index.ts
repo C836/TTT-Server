d076415df40f5854e9d21ad8e968ab792b499b3f
C836 <gl.sepiol@gmail.com>
2022-06-10 22:34:13 -0300

    FEAT: Configuração inicial do servidor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello world")
+import express from "express"
+import cors from "cors"
+import * as http from "http"
+import { Server } from "socket.io"
+
+const app = express()
+const server = http.createServer(app)
+const PORT = process.env.PORT || 3010
+
+app.use(cors())
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PATCH"]
+    }
+})
+
+server.listen(PORT, () => {
+    console.log("ON Porta " + PORT)
+})
