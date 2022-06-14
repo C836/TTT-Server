dca22b23b6478d571e434548cfcf4bfd2f11c43f
C836 <gl.sepiol@gmail.com>
2022-06-13 23:50:11 -0300

    FIX: Emitindo registro de jogadas para ambos os jogadores

--- src/actions/game.ts
+++ src/actions/game.ts
@@ -8,6 +8,7 @@ export default function game(socket, io) {
   });
 
   socket.on("send_position", (data) => {
+    socket.emit("register_position", data);
     socket.to(data.room).emit("receive_position", data);
   });
 }
