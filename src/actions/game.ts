34edb66720dd0399f5c2869c1d51415b293d6dba
C836 <gl.sepiol@gmail.com>
2022-06-14 17:16:05 -0300

    FEAT: Exibindo vencedor do jogo

--- src/actions/game.ts
+++ src/actions/game.ts
@@ -1,4 +1,8 @@
+import { verify, pattern_3x3, pattern_4x4 } from "./win_conditions.js"
+
 export default function game(socket, io) {
+  let movements = []
+
   socket.on("choose_player", async (data) => {
     const sockets = await io.in(data.room).allSockets();
     const connected_users = Array.from(sockets);
@@ -10,5 +14,12 @@ export default function game(socket, io) {
   socket.on("send_position", (data) => {
     socket.emit("register_position", data);
     socket.to(data.room).emit("receive_position", data);
+
+    movements.push(data.position)
+    let pattern = pattern_3x3
+
+    verify({pattern, movements}, (win) => {
+      io.in(data.room).emit("win", { winner: socket.id });
+    })
   });
 }
